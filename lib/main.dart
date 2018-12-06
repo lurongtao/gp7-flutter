aa608ca8cdeb0b40471d186273de30124def20ab
lurongtao <lurongtao@126.com>
2018-12-06 11:42:43 +0800

    drawer-2

--- lib/main.dart
+++ lib/main.dart
@@ -72,12 +72,31 @@ class Home extends StatelessWidget {
             child: ListView(
               padding: EdgeInsets.zero,
               children: <Widget>[
-                DrawerHeader(
-                  child: Text('HEADER'),
+                // DrawerHeader(
+                //   child: Text('HEADER'),
+                //   decoration: BoxDecoration(
+                //     color: Colors.grey[100]
+                //   ),
+                // ),
+                UserAccountsDrawerHeader(
+                  accountName: Text('Felixlu', style: TextStyle(fontWeight: FontWeight.bold)),
+                  accountEmail: Text('lurongtao@1000phone.com'),
+                  currentAccountPicture: CircleAvatar(
+                    backgroundImage: NetworkImage('http://www.mobiletrain.org/images/qf-2018-h5/c11-teach-22.jpg'),
+                  ),
                   decoration: BoxDecoration(
-                    color: Colors.grey[100]
+                    color: Colors.yellow[400],
+                    image: DecorationImage(
+                      image: NetworkImage('http://d.hiphotos.baidu.com/image/h%3D300/sign=e6cb69522534349b6b066885f9eb1521/91ef76c6a7efce1b5ef04082a251f3deb58f659b.jpg'),
+                      fit: BoxFit.cover,
+                      colorFilter: ColorFilter.mode(
+                        Colors.yellow[400].withOpacity(0.6),
+                        BlendMode.hardLight
+                      )
+                    )
                   ),
                 ),
+                
                 ListTile(
                   title: Text('message', textAlign: TextAlign.right),
                   trailing: Icon(Icons.message, size: 22.0, color: Colors.black12),
